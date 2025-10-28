303e42c15b790f3a66ef7dbc683dee2209241c17
mjones2504 <70037194+mjones2504@users.noreply.github.com>
2025-10-27 20:51:54 -0400

    Change SERVER_URL to HTTPS in migration command
    Update SERVER_URL to use HTTPS for migration commands.

--- apps/OpenSignServer/index.js
+++ apps/OpenSignServer/index.js
@@ -234,8 +234,8 @@ if (!process.env.TESTING) {
     // console.log('isWindows', isWindows);
     createContactIndex();
     const migrate = isWindows
-      ? `set APPLICATION_ID=${serverAppId}&& set SERVER_URL=http://localhost:${port}${mountPath}&& set MASTER_KEY=${process.env.MASTER_KEY}&& npx parse-dbtool migrate`
-      : `APPLICATION_ID=${serverAppId} SERVER_URL=http://localhost:${port}${mountPath} MASTER_KEY=${process.env.MASTER_KEY} npx parse-dbtool migrate`;
+      ? `set APPLICATION_ID=${serverAppId}&& set SERVER_URL=https://localhost:${port}${mountPath}&& set MASTER_KEY=${process.env.MASTER_KEY}&& npx parse-dbtool migrate`
+      : `APPLICATION_ID=${serverAppId} SERVER_URL=https://localhost:${port}${mountPath} MASTER_KEY=${process.env.MASTER_KEY} npx parse-dbtool migrate`;
     setTimeout(() => {
     exec(migrate, (error, stdout, stderr) => {
       if (error) {
